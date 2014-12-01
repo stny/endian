5a154942cfd11de7e9f82feea7ef07ff0ef6d63a
Naoya Sato <s@tonaoya.com>
2014-12-01 18:13:47 +0900

    gofmt -w

--- endian.go
+++ endian.go
@@ -7,7 +7,7 @@ var MiddleEndian middleEndian
 type middleEndian struct{}
 
 func (middleEndian) Uint16(b []byte) uint16 {
-	return uint16(b[0]) | uint16(b[1]) << 8
+	return uint16(b[0]) | uint16(b[1])<<8
 }
 
 func (middleEndian) PutUint16(b []byte, v uint16) {
@@ -16,7 +16,7 @@ func (middleEndian) PutUint16(b []byte, v uint16) {
 }
 
 func (middleEndian) Uint32(b []byte) uint32 {
-	return uint32(b[2]) | uint32(b[3]) << 8 | uint32(b[0]) << 16 | uint32(b[1]) << 24
+	return uint32(b[2]) | uint32(b[3])<<8 | uint32(b[0])<<16 | uint32(b[1])<<24
 }
 
 func (middleEndian) PutUint32(b []byte, v uint32) {
@@ -27,8 +27,8 @@ func (middleEndian) PutUint32(b []byte, v uint32) {
 }
 
 func (middleEndian) Uint64(b []byte) uint64 {
-	return uint64(b[6]) | uint64(b[7]) << 8 | uint64(b[4]) << 16 | uint64(b[5]) << 24 |
-	uint64(b[2]) << 32 | uint64(b[3]) << 40 | uint64(b[0]) << 48 | uint64(b[1]) << 56
+	return uint64(b[6]) | uint64(b[7])<<8 | uint64(b[4])<<16 | uint64(b[5])<<24 |
+		uint64(b[2])<<32 | uint64(b[3])<<40 | uint64(b[0])<<48 | uint64(b[1])<<56
 }
 
 func (middleEndian) PutUint64(b []byte, v uint64) {
